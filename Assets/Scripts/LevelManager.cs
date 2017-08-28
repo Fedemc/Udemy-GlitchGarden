9a7bbbf6123bb9fc2f6f648f110acfb7e2cf58fa
Fede <fedemc@gmail.com>
2017-08-28 00:32:13 -0300

    Agregado splashscreen y main menu temporal

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    public float AutoLoadNextLevelAfter;
+
+    private void Start()
+    {
+        Invoke("LoadNextLevel", AutoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name)
+    {
+        //Debug.Log("Level load requested for: "+name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitLevel()
+    {
+        Debug.Log("Level quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    
+}
