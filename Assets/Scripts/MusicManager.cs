471b078d41a8b44d764cfae375b2e4f4e7546056
Fede <fedemc@gmail.com>
2017-09-07 00:13:06 -0300

    Mejorado sistema de música, agregadas escenas Options, Level01, Win, Lose. Agregada música para level01

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+    public AudioClip[] LevelMusicChangeArray;
+
+
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Dont destroy on load: " + name);
+    }
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    private void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = LevelMusicChangeArray[level];
+        Debug.Log("Playing: " + LevelMusicChangeArray[level]);
+        
+        if(thisLevelMusic) //Si esta cargada la musica
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
