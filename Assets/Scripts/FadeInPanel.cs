e562448a2faba5895995f11f77facf46270c99d8
FEDERICO-MONTOL\federico.montoli <fedemc@gmail.com>
2017-09-15 19:30:08 -0300

    Agregado FadeIn a Main Menu y textura de pasto a las stages

---
+++ Assets/Scripts/FadeInPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInPanel : MonoBehaviour {
+    public float FadeInTime;
+
+    private Image fadePanel;
+    private Color currentColor= Color.black;
+
+
+	// Use this for initialization
+	void Start () {
+        fadePanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Time.timeSinceLevelLoad < FadeInTime)
+        {
+            //Aca hay que hacer el fade in
+            float alphaCange = Time.deltaTime / FadeInTime;
+            currentColor.a -= alphaCange;
+            fadePanel.color = currentColor;
+        }
+        else
+        {
+            gameObject.SetActive (false);
+        }
+        
+    }
+}
