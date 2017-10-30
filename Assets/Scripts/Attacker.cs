760ccdb2c01248848eb6b4a9de913e261f05fda7
Fede-HomePC <fedemc@gmail.com>
2017-10-29 22:24:57 -0300

    Animation events en lizard y fox. Colliders en defenders y attackers

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class Attacker : MonoBehaviour {
 
     [Range(-1f, 1.5f)]
-    public float WalkSpeed;
+    public float CurrentSpeed;
 
 	// Use this for initialization
 	void Start ()
     {
+        Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidBody.isKinematic = true;
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.Translate(Vector3.left * WalkSpeed * Time.deltaTime);
+        transform.Translate(Vector3.left * CurrentSpeed * Time.deltaTime);
 		
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(name + " trigger enter");
+    }
+
+    public void ChangeSpeed(float speed)
+    {
+        CurrentSpeed = speed;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        Debug.Log(name + " deals " + damage + " damage");
+    }
 }
