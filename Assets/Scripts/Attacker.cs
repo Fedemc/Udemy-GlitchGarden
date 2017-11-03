1b4ce907d621b28b43619b5d6c0c85ba6b7675f7
Fede-HomePC <fedemc@gmail.com>
2017-11-02 23:43:54 -0300

    Agregados scripts para Fox y Lizard, llaman metodos de Attacker (lo heredan).

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -2,25 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody2D))]
 public class Attacker : MonoBehaviour {
 
     [Range(-1f, 1.5f)]
     public float CurrentSpeed;
 
-	// Use this for initialization
-	void Start ()
-    {
-        Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
-        myRigidBody.isKinematic = true;
+    private GameObject currentTarget;
 
-		
-	}
-	
 	// Update is called once per frame
 	void Update ()
     {
         transform.Translate(Vector3.left * CurrentSpeed * Time.deltaTime);
-		
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -36,5 +29,10 @@ public class Attacker : MonoBehaviour {
     public void StrikeCurrentTarget(float damage)
     {
         Debug.Log(name + " deals " + damage + " damage");
+    }
+
+    public void Attack(GameObject obj)
+    {
+        currentTarget = obj;
     }
 }
---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Attacker))]
+
+public class Fox : MonoBehaviour {
+
+    private Animator anim;
+    private Attacker attacker;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        attacker = GetComponent<Attacker>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+
+        //Me guardo el gameobject contra el que estoy colisionando, lo guardo en "obj"
+        GameObject obj = collider.gameObject;
+        
+        //Si no colisiono contra un miembro de Defender
+        if(!obj.GetComponent<Defender>())
+        {
+            return;
+        }
+
+        if(obj.GetComponent<Stone>())
+        {
+            anim.SetTrigger("jumpTrigger");
+        }
+        else
+        {
+            anim.SetBool("isAttacking", true);
+            attacker.Attack(obj);
+        }
+
+        Debug.Log("Fox collided with " + collider);
+    }
+}
